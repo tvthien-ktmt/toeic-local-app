762ce8278f75fe55b6e63f572e8f9b361daf9369
vinhthien2810 <thien28102004@gmail.com>
2026-08-22 20:49:19 +0700

    chore: clean repository and untrack internal development docs and skills

--- python-backend-dev-rules/audit.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-if exist "C:\Program Files\Git\bin\bash.exe" (
-    "C:\Program Files\Git\bin\bash.exe" "%~dp0audit-rules.sh" "%~dp0..\backend\app"
-) else (
-    bash "%~dp0audit-rules.sh" "%~dp0..\backend\app"
-)
-endlocal
